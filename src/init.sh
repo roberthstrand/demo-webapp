9b623cae63c5a479600766ab1f465e527d61faff
Roberth Strand <me@robstr.dev>
2024-04-02 19:08:58 +0200

    feat: added background color
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- src/init.sh
+++ src/init.sh
@@ -4,7 +4,7 @@ cat << EOM > /usr/share/nginx/html/index.html
   <head>
     <title>Web "app"</title>
   </head>
-  <body>
+  <body bgcolor="${BGCOLOR}">
   <div style="width:800px;margin: 0 auto">
   <!-- BEGIN -->
   <center><h2>Hello from ${POD} on ${NODE}</h2></center>
